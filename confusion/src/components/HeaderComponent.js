bfde30ab881708d0a6b4c9043516a2ff59bb2cc3
santiquiroz <squirozu@unal.edu.co>
2020-04-29 10:47:01 -0500

    controlled form validation

--- confusion/src/components/HeaderComponent.js
+++ confusion/src/components/HeaderComponent.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
     constructor(props) {
         super(props);
     
-        this.toggleNav = this.toggleNav.bind(this);
         this.state = {
-          isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         };
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
       }
 
       toggleNav() {
@@ -18,6 +22,11 @@ class Header extends Component {
         });
       }
 
+      toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+          });
+      }
     render() {
         return(
             <div>
@@ -53,6 +62,12 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                    
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
