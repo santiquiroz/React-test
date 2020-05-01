e14e58866fe78afcecbb0b897daa780fb97e6907
santiquiroz <squirozu@unal.edu.co>
2020-05-01 09:39:21 -0500

    React Animations

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 
 const mapStateToProps = state => {
@@ -23,11 +24,11 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
     postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
-    fetchDishes: () => { dispatch(fetchDishes())},
-    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
+    fetchDishes: () => { dispatch(fetchDishes()) },
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback')) },
     fetchComments: () => dispatch(fetchComments()),
     fetchPromos: () => dispatch(fetchPromos())
-  });
+});
 
 
 class Main extends Component {
@@ -46,43 +47,49 @@ class Main extends Component {
 
     render() {
         const HomePage = () => {
-            return(
-                <Home 
-              dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-              dishesLoading={this.props.dishes.isLoading}
-              dishesErrMess={this.props.dishes.errMess}
-              promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
-              promosLoading={this.props.promotions.isLoading}
-              promosErrMess={this.props.promotions.errMess}
-              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-          />
+            return (
+                <Home
+                    dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                    promosLoading={this.props.promotions.isLoading}
+                    promosErrMess={this.props.promotions.errMess}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
-          }
-          const DishWithId = ({match}) => {
-            return(
-                <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-            isLoading={this.props.dishes.isLoading}
-            errMess={this.props.dishes.errMess}
-            comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-            commentsErrMess={this.props.comments.errMess}
-            postComment={this.props.postComment}
-          />
+        }
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}
+                    comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    commentsErrMess={this.props.comments.errMess}
+                    postComment={this.props.postComment}
+                />
             );
-          };
+        };
 
 
         return (
             <div>
                 <Header />
-                <Switch>
-                    <Route path="/home" component={HomePage} />
-                    <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}></Menu>} />
-                    <Route path='/menu/:dishId' component={DishWithId} />
-                    <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}></Contact>} />
-                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders}></About>} />
-                    <Redirect to="/home"></Redirect>
-                </Switch>
-                <Footer />
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                        <Switch>
+                            <Route path="/home" component={HomePage} />
+                            <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}></Menu>} />
+                            <Route path='/menu/:dishId' component={DishWithId} />
+                            <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}></Contact>} />
+                            <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders}></About>} />
+                            <Redirect to="/home"></Redirect>
+                        </Switch>
+                    </CSSTransition>
+
+                    <Footer />
+                </TransitionGroup>
+
             </div>
         );
     }
