9702b673d35525a36adeb29140cc6439ba985a44
santiquiroz <squirozu@unal.edu.co>
2020-04-30 19:27:31 -0500

    fetch postComments

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -34,7 +34,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -126,7 +126,7 @@ function RenderDish({ dish }) {
 
 
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
 
 
     if (comments != null) {
@@ -142,7 +142,7 @@ function RenderComments({comments, addComment, dishId}) {
                         );
                     })}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment}></CommentForm>
+                <CommentForm dishId={dishId} postComment={postComment}></CommentForm>
             </div>
 
         );
@@ -196,7 +196,7 @@ const DishDetail = (props) => {
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         <RenderComments comments={props.comments} 
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId = {props.dish.id}/>
                     </div>
                 </div>
--- confusion/src/redux/ActionCreators.js
+++ confusion/src/redux/ActionCreators.js
@@ -1,67 +1,99 @@
 import * as ActionTypes from './ActionTypes';
 import { baseUrl } from '../shared/baseUrl';
 
-export const addComment = (dishId, rating, author, comment) => ({
-    type: ActionTypes.ADD_COMMENT,
-    payload: {
-        dishId: dishId,
-        rating: rating,
-        author: author,
-        comment: comment
-    }
+
+
+export const addComment = (comment) => ({
+  type: ActionTypes.ADD_COMMENT,
+  payload: comment
 });
 
 export const fetchDishes = () => (dispatch) => {
 
-    dispatch(dishesLoading(true));
+  dispatch(dishesLoading(true));
 
-    return fetch(baseUrl + 'dishes')
+  return fetch(baseUrl + 'dishes')
     .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
-      },
+      if (response.ok) {
+        return response;
+      } else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }
+    },
       error => {
-            var errmess = new Error(error.message);
-            throw errmess;
+        var errmess = new Error(error.message);
+        throw errmess;
       })
     .then(response => response.json())
     .then(dishes => dispatch(addDishes(dishes)))
     .catch(error => dispatch(dishesFailed(error.message)));
 }
 
+export const postComment = (dishId, rating, author, comment) => (dispatch) => {
+
+  const newComment = {
+    dishId: dishId,
+    rating: rating,
+    author: author,
+    comment: comment
+  };
+  newComment.date = new Date().toISOString();
+
+  return fetch(baseUrl + 'comments', {
+    method: "POST",
+    body: JSON.stringify(newComment),
+    headers: {
+      "Content-Type": "application/json"
+    },
+    credentials: "same-origin"
+  })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }
+    },
+      error => {
+        throw error;
+      })
+    .then(response => response.json())
+    .then(response => dispatch(addComment(response)))
+    .catch(error => { console.log('post comments', error.message); alert('Your comment could not be posted\nError: ' + error.message); });
+};
+
 export const dishesLoading = () => ({
-    type: ActionTypes.DISHES_LOADING
+  type: ActionTypes.DISHES_LOADING
 });
 
 export const dishesFailed = (errmess) => ({
-    type: ActionTypes.DISHES_FAILED,
-    payload: errmess
+  type: ActionTypes.DISHES_FAILED,
+  payload: errmess
 });
 
 export const addDishes = (dishes) => ({
-    type: ActionTypes.ADD_DISHES,
-    payload: dishes
+  type: ActionTypes.ADD_DISHES,
+  payload: dishes
 });
 
-export const fetchComments = () => (dispatch) => {    
-    return fetch(baseUrl + 'comments')
+export const fetchComments = () => (dispatch) => {
+  return fetch(baseUrl + 'comments')
     .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
-      },
+      if (response.ok) {
+        return response;
+      } else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }
+    },
       error => {
-            var errmess = new Error(error.message);
-            throw errmess;
+        var errmess = new Error(error.message);
+        throw errmess;
       })
     .then(response => response.json())
     .then(comments => dispatch(addComments(comments)))
@@ -69,34 +101,34 @@ export const fetchComments = () => (dispatch) => {
 };
 
 export const commentsFailed = (errmess) => ({
-    type: ActionTypes.COMMENTS_FAILED,
-    payload: errmess
+  type: ActionTypes.COMMENTS_FAILED,
+  payload: errmess
 });
 
 
 export const addComments = (comments) => ({
-    type: ActionTypes.ADD_COMMENTS,
-    payload: comments
+  type: ActionTypes.ADD_COMMENTS,
+  payload: comments
 });
 
 
 export const fetchPromos = () => (dispatch) => {
-    
-    dispatch(promosLoading());
 
-    return fetch(baseUrl + 'promotions')
+  dispatch(promosLoading());
+
+  return fetch(baseUrl + 'promotions')
     .then(response => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
-          error.response = response;
-          throw error;
-        }
-      },
+      if (response.ok) {
+        return response;
+      } else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }
+    },
       error => {
-            var errmess = new Error(error.message);
-            throw errmess;
+        var errmess = new Error(error.message);
+        throw errmess;
       })
     .then(response => response.json())
     .then(promos => dispatch(addPromos(promos)))
@@ -104,16 +136,16 @@ export const fetchPromos = () => (dispatch) => {
 }
 
 export const promosLoading = () => ({
-    type: ActionTypes.PROMOS_LOADING
+  type: ActionTypes.PROMOS_LOADING
 });
 
 
 export const promosFailed = (errmess) => ({
-    type: ActionTypes.PROMOS_FAILED,
-    payload: errmess
+  type: ActionTypes.PROMOS_FAILED,
+  payload: errmess
 });
 
 export const addPromos = (promos) => ({
-    type: ActionTypes.ADD_PROMOS,
-    payload: promos
+  type: ActionTypes.ADD_PROMOS,
+  payload: promos
 });
